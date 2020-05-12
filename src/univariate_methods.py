fb6084e18763b8e10010c9676e4d304cca0d49e1
SubratoChakravorty <chakravorty.subrato@gmail.com>
2020-05-11 21:22:47 -0700

    added return fields

---
+++ src/univariate_methods.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+
+def return_fields(file_loc="../data/student_data.csv"):
+    '''
+    returns the field values from the dataset
+    :return:
+    '''
+
+    isinstance(file_loc, str)
+
+    df = pd.read_csv(file_loc)
+
+    val_details = {'STU_ID':'Student ID', 'X1RACE': 'Student Race', 'X1SEX': 'Student Sex', 'X1SES':'Socioeconomic status',
+           'X1SCIEFF':'Student Science Self-efficacy', 'N1COURSE': 'Science Course', 'X1SCIID': 'Scale of student\'s science identity',
+           'X1SCIUTI':'Scale of student\'s science utility', 'X1SCIINT': 'Scale of student\'s interest in fall 2009 science course',
+           'S1TEFRNDS': 'Time/effort in math/science means not enough time with friends', 'S1TEACTIV':'Time/effort in math/science means not enough time \
+            for extracurriculars', 'S1TEPOPULAR':'Time/effort in math/science means 9th grader won\'t be popular', 'S1TEMAKEFUN':'Time/effort in math/science\
+             means people will make fun of 9th grader','X1CONTROL':'School Control', 'X1LOCALE': 'School Locale (Urbanicity)',
+            'N1SEX': 'Science Teacher’s Sex', 'X1TSRACE': 'Science Teacher’s Race', 'X1TSCERT': 'Science teacher\'s science teaching certification',
+           'N1HIDEG':'Science teacher\'s highest degree', 'N1SCIJOB': 'Science teacher held science-related prior to becoming a teacher',
+           'N1ALTCERT': 'Science teacher entered profession through alternative certification program', 'N1SCIYRS912': 'Years science teacher has taught high school science',
+           'N1GROUP': 'Science teacher has students work in small groups', 'N1INTEREST': 'increasing students\' interest in science',
+           'N1CONCEPTS':'teaching basic science concepts', 'N1TERMS': 'important science terms/facts N1SKILLS science process/inquiry skills',
+            'S1STCHVALUES': '‘9th grader\'s fall 2009 science teacher values/listens to students\' ideas', 'S1STCHRESPCT': '9th grader\'s fall 2009 science\
+            teacher treats students with respect', 'S1STCHFAIR': '9th grader\'s fall 2009 science teacher treats every\
+            student fairly', 'S1STCHCONF': '‘9th grader\'s fall 09 science teacher thinks all students can be successful',
+           'S1STCHMISTKE': '9th grader\'s fall 09 science teacher think mistakes OK if students learn', 'X3TGPAENG': 'English GPA',
+           'X3TGPAMAT' : 'Mathematics GPA', 'X3TGPASCI': 'Science GPA'}
+
+    res = dict()
+    for k, v in val_details.items():
+        if k in df.columns:
+            res[k] = v
+
+    return res
+
+
+
+# print(return_fields())
+
