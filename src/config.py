10b7cb21a5bc1c690b0d554eb915cc6c03f65504
Ian Pegg <ianpegg9@gmail.com>
2020-05-13 10:59:18 -0700

    Added a config file to help make file paths consistent

---
+++ src/config.py
@@ -0,0 +1,4 @@
+import os.path as path
+
+school_data_file = path.join(path.dirname(path.dirname(__file__)), 'data/school_data.csv')
+student_data_file = path.join(path.dirname(path.dirname(__file__)), 'data/student_data.csv')
