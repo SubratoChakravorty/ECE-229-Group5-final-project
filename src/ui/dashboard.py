1da15b88e41df36cec89e70672126acc8e39548f
wangjinjie722 <wangjinjie722@gmail.com>
2020-05-15 00:37:04 -0700

    change the layout: cancel tab add univariate analysis

--- src/ui/dashboard.py
+++ src/ui/dashboard.py
@@ -39,8 +39,26 @@ colors = {
     'text': '#7FDBFF'
 }
 # load first page markdown file
-with open("../txt/Readme.md", "r") as f:
-    markdown_first_page = f.read()
+# with open("../txt/Readme.md", "r") as f:
+#     markdown_first_page = f.read()
+
+categories = return_fields('../../data/student_data.csv')
+col_types = {}
+for cate in categories:
+    col_types[cate] = cate
+col_options = [dict(label=x, value=x) for x in categories]
+print(col_types)
+
+well_status_options = [
+    {"label": str(col_types[well_status]), "value": str(well_status)}
+    for well_status in col_types
+]
+
+well_type_options = [
+    {"label": str(col_types[well_type]), "value": str(well_type)}
+    for well_type in col_types
+]
+# print(WELL_STATUSES)
 
 app = dash.Dash(__name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}])
 
@@ -60,21 +78,6 @@ introduction_tab = dcc.Tab(
     label='About',
     value='what-is',
     children=html.Div(className='control-tab', children=[
-
-        # html.H4(className='what-is', children="What is Circos?"),
-        # html.P('Our user: a secondary education policy-maker interested in '
-        #        'boosting the number of students who choose STEM courses, majors, '
-        #        'and careers after leaving high-school.'),
-        # html.P('This user has a very clear, and measurable goal, but lacks '
-        #        'direction. They do not have a concrete data-driven understanding'
-        #        ' of the factors that lead students to choose science, technology,'
-        #        ' engineering, and mathematics courses, majors, and careers.'),
-        # html.P('This study employs public-use data from the High School Longitudinal'
-        #        ' Study of 2009 (HSLS:09). One important difference between HSLS:09 '
-        #        'and previous studies is its focus on STEM education; one specific '
-        #        'goal of the study is to gain an understanding of the factors that '
-        #        'lead students to choose science, technology, engineering, and '
-        #        'mathematics courses, majors, and careers.'),
 
         html.H4(className='how', children="How to use this dashboard"),
 
@@ -111,35 +114,34 @@ introduction_tab = dcc.Tab(
         html.Br()
 
     ])
-    # TODO: too ugly lol
 )
 
-categories = return_fields('../../data/student_data.csv')
-col_options = [dict(label=x, value=x) for x in categories]
-
-explore_tab = dcc.Tab(
-    label="Explore",
-    children=[
-        html.H1("Explore the Data"), html.P("Click a category on the inner plot to filter"),
-        html.Div(
-            [
-                html.P(["Select categories:", dcc.Dropdown(id='category_selector', options=col_options, multi=True)]),
-                html.P(["Select score:", dcc.Dropdown(id='color_var_selector', options=col_options)]),
-            ],
-            style={"width": "25%", "float": "left"}
-        ),
-        dcc.Graph(id="graph",
-                  style={"width": "75%", "display": "inline-block"},
-                  animate=False),
-    ]
-)
-
-inspect_tab = dcc.Tab(
-    label="Inspect",
-    children=[
-        html.H1("Univariate Analysis")
-    ]
-)
+inspect_tab = html.Div([
+    dcc.Tab(
+        label="Inspect",
+        children=[
+            html.H1("Univariate Analysis"),
+            html.P("Click a category on the inner plot to filter"),
+            html.Div(
+                [
+                    html.P(["Select continous_var:", dcc.Dropdown(id='continous_var_selector', options=col_options, multi=True)]),
+                ],
+                style={"width": "25%", "float": "left"}
+            ),
+            dcc.Graph(id="graph2",
+                    style={"width": "75%", "display": "inline-block"},
+                    animate=False)
+        ]
+    ),
+        dcc.Slider(
+        id='continous-slider',
+        min=0,
+        max=100,
+        value=0,
+        #marks={str(year): str(year) for year in df['year'].unique()},
+        step=None
+    ),
+])
 
 insights_tab = dcc.Tab(
     label="Insights",
@@ -205,51 +207,150 @@ app.layout = html.Div(
             style={"margin-bottom": "25px"},
         ),
 
-        ######################################################< TAG PART >##################################################
-        
-        dcc.Tabs(
+        ######################################################< TAG1 PART >##################################################
+    
+        html.Div(
             [
-                introduction_tab,
-                explore_tab,
-                inspect_tab,
+                html.H4(className='how', children="How to use this dashboard"),
+
+                html.P('1. Explore'
+                        'In this section, feel free to explore the distribution of data. We provide pie chart, xx chart, ...etc.'),
+                html.P(
+                    '2. Inspect'
+                        'We provide univariate analysis in this part. You can find how single variable affects your self-efficiency.'),
+                html.P(
+                    '3. Insights'
+                        'Multivariate statistical analysis can be found here, which helps you to gain insights on the data and provides advice for yourself.'),
+                
+                # html.Div([
+                #     'For a look into Circos and the Circos API, please visit the '
+                #     'original repository ',
+                #     html.A('here', href='https://github.com/nicgirault/circosJS)'),
+                #     '.'
+                # ]),
+                # html.H4(className='what-is', children="What is Circos?"),
+
+                html.H4(className='Dataset', children="Dataset"), 
+                html.P('This study employs public-use data from the High School Longitudinal Study of 2009 (HSLS:09). One important difference'
+                    'between HSLS:09 and previous studies is its focus on STEM education; one specific goal of the study is to gain an '
+                    'understanding of the factors that lead students to choose science, technology, engineering, and mathematics courses, majors, and careers.'),
+                
+                html.P("Dataset can be downloaded by clicking: "),
+                html.Div([
+                    'Reference: ',
+                    html.A('Dataset',
+                            href='https://nces.ed.gov/EDAT/Data/Zip/HSLS_2016_v1_0_CSV_Datasets.zip)')
+                ]),
+                html.H4(className='author', children="Author"),
+                
+                html.Br()
+            ],
+            className="pretty_container",
+        ),
+
+        ######################################################< TAG2 PART >##################################################
+        
+        html.Div(
+            [
+                html.H1("Explore the Data"), html.P("Click a category on the inner plot to filter"),
+                html.Div(
+                    [
+                        html.P(["Select categories:", dcc.Dropdown(id='category_selector', options=col_options, multi=True)]),
+                        html.P(["Select score:", dcc.Dropdown(id='color_var_selector', options=col_options)]),
+                    ],
+                    style={"width": "25%", "float": "left"}
+                ),
+                dcc.Graph(id="graph1",
+                        style={"width": "75%", "display": "inline-block"},
+                        animate=False)
+            ],
+            className="pretty_container",
+        ),
+
+        ######################################################< TAG3 PART >##################################################
+
+        html.Div(
+            [
+                html.Div(
+                    [
+                        html.H1("Univariate Analysis"),
+                        html.P("Select categories:", className="control_label"),
+                        dcc.Dropdown(
+                            id="well_statuses",
+                            options=well_status_options,
+                            multi=True,
+                            value=list(WELL_STATUSES.keys()),
+                            className="dcc_control",
+                        ),
+                        html.P("Select score:", className="control_label"),
+                        dcc.Dropdown(
+                            id="well_types",
+                            options=well_type_options,
+                            multi=True,
+                            value=list(WELL_TYPES.keys()),
+                            className="dcc_control",
+                        ),
+                    ],
+                    className="pretty_container four columns",
+                    id="cross-filter-options",
+                ),
+                html.Div(
+                    [
+                        html.Div(
+                            [
+                                html.Div(
+                                    [html.H6(id="well_text"), html.P("No. of Wells")],
+                                    id="wells",
+                                    className="mini_container",
+                                ),
+                                html.Div(
+                                    [html.H6(id="gasText"), html.P("Gas")],
+                                    id="gas",
+                                    className="mini_container",
+                                ),
+                                html.Div(
+                                    [html.H6(id="oilText"), html.P("Oil")],
+                                    id="oil",
+                                    className="mini_container",
+                                ),
+                                html.Div(
+                                    [html.H6(id="waterText"), html.P("Water")],
+                                    id="water",
+                                    className="mini_container",
+                                ),
+                            ],
+                            id="info-container",
+                            className="row container-display",
+                        ),
+                        html.Div(
+                            [dcc.Graph(id="count_graph")],
+                            id="countGraphContainer",
+                            className="pretty_container",
+                        ),
+                    ],
+                    id="right-column",
+                    className="eight columns",
+                ),
+            ],
+            className="row flex-display",
+        ),
+
+        ######################################################< TAG4 PART >##################################################
+
+        html.Div(
+            [
                 insights_tab,
             ],
         ),
         
         # TODO: more graphs
 
-        # html.Div(
-        #     [
-        #         html.Div(
-        #             [dcc.Graph(id="main_graph")],
-        #             className="pretty_container seven columns",
-        #         ),
-        #         html.Div(
-        #             [dcc.Graph(id="individual_graph")],
-        #             className="pretty_container five columns",
-        #         ),
-        #     ],
-        #     className="row flex-display",
-        # ),
-        # html.Div(
-        #     [
-        #         html.Div(
-        #             [dcc.Graph(id="pie_graph")],
-        #             className="pretty_container seven columns",
-        #         ),
-        #         html.Div(
-        #             [dcc.Graph(id="aggregate_graph")],
-        #             className="pretty_container five columns",
-        #         ),
-        #     ],
-        #     className="row flex-display",
-        # ),
     ],
     id="mainContainer",
     style={"display": "flex", "flex-direction": "column"},
 )
 
-@app.callback(Output('graph', 'figure'), [Input('category_selector', 'value'), Input('color_var_selector', 'value')])
+@app.callback(Output('graph1', 'figure'), [Input('category_selector', 'value'), Input('color_var_selector', 'value')])
 def make_sunburst(fields, color_var):
     """
     Callback to generate the sunburst figure based on the selected categorical input fields and the desired 
