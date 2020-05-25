7c3e554bc05172fa2181300e6035cd43d45ca384
Ian Pegg <ianpegg9@gmail.com>
2020-05-24 18:54:55 -0700

    fixed box plot test
    made pytest headless by default

--- src/test/test_ui.py
+++ src/test/test_ui.py
@@ -42,13 +42,12 @@ def test_ui003_second_univariate_plot_box_gives_select_score_prompt(dash_duo):
 def test_ui003_second_univariate_plot_box(dash_duo):
     dash_duo.start_server(app)
     dash_duo.wait_for_contains_text('#plot_selector', "frequency plot", timeout=2)
-    dash_duo.select_dcc_dropdown('#expl_category_selector', index=0)
     dash_duo.select_dcc_dropdown('#plot_selector', index=0)
-    dash_duo.select_dcc_dropdown('#expl_continuous_selector', index=0)
 
     short_text = dash_duo.find_element('#expl_continuous_selector').text
-    short_text, _ = re.compile(r"[^a-zA-Z ]").subn('', short_text)
+    short_text, _ = re.compile(r"[^a-zA-Z \-']").subn('', short_text)
     dash_duo.wait_for_contains_text('#second_explore_plot', short_text, timeout=3)
+
 
 def test_ui004_histogram_plot(dash_duo):
     dash_duo.start_server(app)
@@ -56,14 +55,17 @@ def test_ui004_histogram_plot(dash_duo):
     dash_duo.wait_for_element('#width_slider', timeout=2)
     dash_duo.wait_for_element('#hist_plot', timeout=2)
 
+
 def test_ui005_four_blocks(dash_duo):
     dash_duo.start_server(app)
     dash_duo.wait_for_element('#info-container', timeout=2)
+
 
 def test_ui006_report(dash_duo):
     dash_duo.start_server(app)
     dash_duo.wait_for_element('#open-xl', timeout=2)
     dash_duo.wait_for_element('#report', timeout=2)
+
 
 def test_ui007_toggle_model(dash_duo):
     dash_duo.start_server(app)
@@ -71,6 +73,7 @@ def test_ui007_toggle_model(dash_duo):
     dash_duo.multiple_click("#close-xl", clicks=1)
     dash_duo.multiple_click("#save-xl", clicks=1)
     # dash_duo.toggle_window()
+
 
 if __name__ == '__main__':
     unittest.main(unittest.TestCase)
